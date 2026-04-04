59b46f72b4d804e568631f80975c888346a2c150
111-Vaishali <vaishali.sunepwar24@pccoepune.org>
2026-04-04 09:29:02 +0530

    Booking System added

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -52,9 +52,9 @@ function Navbar() {
             </ul>
           )}
         </li>
-        {/* <li>
+        <li>
           <Link to="/booking" style={styles.link}>Book Now</Link>
-        </li> */}
+        </li>
 
         <li>
           <Link to="/feedback" style={styles.link}>Feedback</Link>
